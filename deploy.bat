d49d87f47bbe933e43eea8c8940a803a66c9094c
fanyongmeng <fanyongmeng@creditease.cn>
2016-05-13 16:06:58 +0800

    修改deploy脚本

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,6 @@ echo off
 echo start deploy!
 del/f/s/q D:\tomcat-7.0.69\webapps\pcweb.war
 rd/s/q D:\tomcat-7.0.69\webapps\pcweb
-copy /y C:\Users\Administrator.PC\Desktop\pcweb\target\pcweb.war D:\tomcat-7.0.69\webapps
+cd target
+copy /y pcweb.war D:\tomcat-7.0.69\webapps
 echo stop deploy!
