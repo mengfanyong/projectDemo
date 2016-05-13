a663bafb8fd13f2108a54bd6636ce2a720deef80
fanyongmeng <fanyongmeng@creditease.cn>
2016-05-13 16:22:18 +0800

    部署脚本

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,6 @@
 echo off  
 echo start deploy!
-del/f/s/q D:\tomcat-7.0.69\webapps\pcweb.war
-rd/s/q D:\tomcat-7.0.69\webapps\pcweb
-cd target
-copy /y pcweb.war D:\tomcat-7.0.69\webapps
+del/f/s/q D:\tomcat-7.0.69\webapps\zhibing_mybatis.war
+rd/s/q D:\tomcat-7.0.69\webapps\zhibing_mybatis
+copy /y C:\Users\Administrator.PC\.jenkins\workspace\demoProject\target\zhibing_mybatis.war D:\tomcat-7.0.69\webapps
 echo stop deploy!
